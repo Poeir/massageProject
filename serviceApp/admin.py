16abfe0d2d68e833e06d9a63fef350f5ab6faffd
Poori Charicha <phuri.c@kkumail.com>
2025-02-25 17:42:47 +0700

    dataUpdated

--- serviceApp/admin.py
+++ serviceApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Service
+
+
+class serviceAdmin(admin.ModelAdmin):
+    list_display = ['ServiceID','Name','Price','Duration']
+    search_fields = ['Name',]
+
+admin.site.register(Service,serviceAdmin)
--- serviceApp/models.py
+++ serviceApp/models.py
@@ -6,7 +6,7 @@ class Service(models.Model):
     Name = models.CharField(max_length=100)
     Description = models.TextField()
     Price = models.DecimalField(max_digits=10, decimal_places=2)
-    Duration = models.TimeField()
+    Duration = models.DecimalField(max_digits=10, decimal_places=2)
     
     def __str__(self):
         return self.Name
