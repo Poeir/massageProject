205f642fd5e5ee875292e3998c019597149cf88f
Poori Charicha <phuri.c@kkumail.com>
2025-02-25 15:22:01 +0700

    boonEdited

--- serviceApp/urls.py
+++ serviceApp/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from serviceApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('homeApp.urls')),
-    path('login/', include('loginApp.urls')),
-    path('service/',include('serviceApp.urls'))
+    path('',views.service)
 ]
