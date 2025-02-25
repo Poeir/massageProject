37b540258eae91803b2c7ef401abf79112cffc3f
Poori Charicha <phuri.c@kkumail.com>
2025-02-25 17:33:20 +0700

    userUpdated

--- serviceApp/models.py
+++ serviceApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    ServiceID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=100)
+    Description = models.TextField()
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Duration = models.TimeField()
+    
+    def __str__(self):
+        return self.Name
